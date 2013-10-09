bd64041bce0b34437c3bad5ba9ab12c04caf7277
adamhammes <adamhammes@centetune.com>
2013-10-09 15:31:57 -0500

    first commit

---
+++ ChapterCreator.py
@@ -0,0 +1,58 @@
+__author__ = 'adamhammes'
+
+import urllib2
+from BeautifulSoup import BeautifulSoup
+
+class Chapter:
+
+
+    def __init__(self, url = None):
+        """
+        Constructs a Chapter from a URL
+        """
+        if url is not None:
+            r = urllib2.urlopen(url)
+            html = r.read()
+            self.soup = BeautifulSoup(html, convertEntities=BeautifulSoup.HTML_ENTITIES)
+            self.setData()
+
+    def fromFileName(self, name):
+        self.soup = BeautifulSoup(open(name), convertEntities=BeautifulSoup.HTML_ENTITIES)
+        self.setData()
+
+    def setNextLink(self):
+        self.nextLink = self.soup.find("a", href = True, text = "Next Chapter")
+
+    def setText(self):
+        self.soup.prettify()
+        chapter = self.soup.find("article")
+        self.text_list = []
+
+        for paragraph in chapter.findAll("p"):
+            text = paragraph.getText().encode("UTF-8")
+            text.strip()
+            self.text_list.append(text)
+
+        self.text_list.pop(0)
+        self.text_list.pop(-1)
+
+    def setTitle(self):
+        raw_title = self.soup.title.string
+        index = raw_title.index("|")
+        self.title = raw_title[:index-1]
+
+    def setData(self):
+        self.setText()
+        self.setTitle()
+        self.setNextLink()
+
+    def writeRawToFile(self, name, delimiter):
+        f = open(name, "w")
+        for paragraph in self.text_list:
+            f.write(delimiter + paragraph)
+
+
+
+
+
+
---
+++ Main.py
@@ -0,0 +1,16 @@
+__author__ = 'adamhammes'
+
+from ChapterCreator import Chapter
+
+c = Chapter()
+c.fromFileName("foo.txt")
+
+print c.nextLink
+
+
+
+
+
+
+
+
