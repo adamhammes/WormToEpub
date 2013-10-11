9a837a6030093d31cba9cd5abf04c98253b418cc
adamhammes <adamhammes@centetune.com>
2013-10-11 09:13:49 -0500

    First time making one chapter

--- Main.py
+++ Main.py
@@ -2,9 +2,19 @@ __author__ = 'adamhammes'
 
 from ChapterCreator import Chapter
 
-c = Chapter("http://parahumans.wordpress.com/category/stories-arcs-1-10/arc-1-gestation/1-01/")
+c = Chapter("http://parahumans.wordpress.com/2011/06/11/1-1/")
 
-print c.nextLink
+f = open("html.html", "w")
+f.write("<meta charset = \"UTF-8\"/>")
+f.write("<h1><center>" + c.title + "</center></h1>\n")
+
+for paragraph in c.text_list:
+    f.write("\t<p>" + paragraph + "</p>\n")
+
+f.write("</h1")
+
+
+
 
 
 
