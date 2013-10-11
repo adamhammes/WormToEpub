6204d65b2e1ab07cd54092e0b123ab36d33e42cf
adamhammes <adamhammes@centetune.com>
2013-10-11 12:41:29 -0500

    First successful write of chapter

--- Main.py
+++ Main.py
@@ -2,17 +2,21 @@ __author__ = 'adamhammes'
 
 from ChapterCreator import Chapter
 
-c = Chapter("http://parahumans.wordpress.com/2011/06/11/1-1/")
-
 f = open("html.html", "w")
 f.write("<meta charset = \"UTF-8\"/>")
-f.write("<h1><center>" + c.title + "</center></h1>\n")
 
-for paragraph in c.text_list:
-    f.write("\t<p>" + paragraph + "</p>\n")
+nextLink = "http://parahumans.wordpress.com/2011/06/11/1-1/"
 
-f.write("</h1")
+while nextLink:
+    c = Chapter(nextLink)
+    print c.arc
+    f.write("<h1><center>" + c.title + "</center></h1>\n")
 
+    for paragraph in c.text_list:
+        f.write("\t" + paragraph + "\n")
+
+    f.write("</h1>")
+    nextLink = c.nextLink
 
 
 
